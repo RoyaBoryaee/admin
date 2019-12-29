118d031291e7bf724cb152c71e948b35f84417cf
RoyaBoryaee <roya.boryaee@gmail.com>
2019-12-29 16:27:08 +0330

    2

---
+++ dist/js/chunk-2d0b5fa6.e8f7d53e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b5fa6"],{"1acc":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{},[t("b-card",{attrs:{"no-body":""}},[t("b-card-header")],1)],1)},c=[],r={name:"Seller_Identity"},l=r,d=t("2877"),u=Object(d["a"])(l,a,c,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0b5fa6.e8f7d53e.js.map
---
+++ dist/js/chunk-2d0c8a58.36eb4051.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c8a58"],{5689:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{},[t("b-card",{attrs:{"no-body":""}},[t("b-card-header")],1)],1)},c=[],r={name:"Seller_Acoount"},l=r,o=t("2877"),u=Object(o["a"])(l,a,c,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0c8a58.36eb4051.js.map
