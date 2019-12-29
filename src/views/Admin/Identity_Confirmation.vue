0f199351e310d042ea1a388878ee08ae331225c3
sara <s.moeini1377@gmail.com>
2019-12-29 20:24:17 +0330

    first commit

---
+++ src/views/Admin/Identity_Confirmation.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="animated fadeIn">
+    <b-row>
+      <b-col>
+        <b-card header="Card title" border-variant="primary" class="bg-gray-100"  col-md-4 style="width:80%; margin-left:auto;margin-right:auto;">
+          <img style="float:left; width:150px; height:150px;" src="img/avatars/7.jpg" class="img-rounded" alt="Cinque Terre" />
+
+          <table
+            class="table bg-white"
+            style="width:70%;margin-left:auto;margin-right:auto;direction:ltr"
+          >
+            <tbody>
+              <tr>
+                <td>
+                  <p >Name</p>
+                </td>
+                <td>
+                  <span class="h3">$routes.params.name</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Email</p>
+                </td>
+                <td>
+                  <span class="h3">$routes.params.email</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Phone Number</p>
+                </td>
+                <td>
+                  <span class="h3">$routes.params.phonenumber</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Locatino</p>
+                </td>
+                <td>
+                  <span class="h3">$routes.params.location</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Activity field</p>
+                </td>
+                <td>
+                  <span class="h3">$routes.params.zctivityfield</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>activityDescription</p>
+                </td>
+                <td>
+                  <span class="h3">h3. Bootstrap heading</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="clearfix"></div>
+          <b-col>
+            <b-button v-click="Approve" variant="primary" class="px-4">Approve</b-button>
+          </b-col>
+        </b-card>
+      </b-col>
+    </b-row>
+    <!--/.row-->
+  </div>
+</template>
+
+<script>
+import { Switch as cSwitch } from "@coreui/vue";
+
+export default {
+  name: "cards",
+  components: {
+    cSwitch
+  },
+  data() {
+    return {
+      show: true,
+      id:this.$routes.params.id,
+      blog: {}
+    };
+  },
+  created() {
+    this.$http.get(" " + this.id).then(function(data) {
+      console.log(data);
+      this.blog = data.body;
+    });
+  },
+  methods:{
+    Approve(){
+     this.blog.approved=true;
+     //post
+    }
+  }
+};
+</script>
+
+<style scoped>
+.fade-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.fade-enter,
+.fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
+
+
