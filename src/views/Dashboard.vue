94b6fe5f9269c7655dfe9732af571cceee4315e1
Niayesh <niayesh.sadeghi@gmail.com>
2020-01-31 18:42:37 +0330

    fix style issues of Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,10 +2,13 @@
   <div>
     <div class="card-deck" style="display: flex; flex-wrap: wrap;">
       <div v-for="itm in result" :key="itm.id" class="card myCard col-3">
-        <img :src="itm.profileImage" class="card-img-top" alt="Profile Image">
+        <img v-if="itm.profileImage === 'string' || itm.profileImage===null" src="@/assets/images/21757743.jpg"
+             class="card-img-top" alt="Profile Image" :id="itm.id" height="60%">
+        <img v-else :src="itm.profileImage" class="card-img-top" alt="Profile Image" :id="itm.id" height="60%">
         <div class="card-body">
-          <h5 class="card-title">نام فروشنده:</h5>
-          <p class="card-text">{{itm.name}}</p>
+          <h5 class="card-title title">نام فروشنده:</h5>
+          <p v-if="itm.name" class="card-text">{{itm.name}}</p>
+          <p v-else class="card-text" style="color: gray">این فروشنده فاقد نام است.</p>
           <div class="btns">
             <div v-if="itm.approved === false" class="approve">
               <button class="btn btn-success" @click="goToApprove(itm.userName)">تایید</button>
@@ -13,6 +16,7 @@
             <button class="btn btn-danger suspend" @click="goToSuspend(itm.userName)">لغو</button>
           </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -55,7 +59,18 @@
               }
             })
             .then(response2 => {
-              this.result = response2.data.items
+              this.result = response2.data.items;
+              for (let i in this.result) {
+                let image_url = this.result[i].profileImage;
+                try {
+                  axios.get(image_url).then(response3 => {
+                    if (response3.status / 100 !== 2)
+                      this.result[i].profileImage = '@/assets/images/21757743.jpg'
+                  })
+                } catch (e) {
+                  this.result[i].profileImage = '@/assets/images/21757743.jpg'
+                }
+              }
             });
         });
     },
@@ -91,9 +106,13 @@
     text-align: right;
   }
 
+  .card-body {
+    padding-bottom: 0;
+  }
+
   .btns {
     text-align: center;
-
+    margin-top: 20px;
   }
 
   .approve {
@@ -109,6 +128,11 @@
     display: inline;
     float: left;
     padding: 5px 25px;
+  }
+
+  .title {
+    font-weight: bold;
+    font-size: 120%;
   }
 
 </style>
